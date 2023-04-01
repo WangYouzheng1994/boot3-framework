455acc0e0ea29da70ca98908809162418afa6e2d
wangyouzheng <353735348@qq.com>
2023-04-01 19:01:49 +0800

    Initial commit

---
+++ src/main/java/com/wyz/boot3framework/Boot3FrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.wyz.boot3framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot3FrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot3FrameworkApplication.class, args);
+    }
+
+}
