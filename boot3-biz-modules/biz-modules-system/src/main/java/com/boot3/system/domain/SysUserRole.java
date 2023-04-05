dccb7cfade2507234460d110a2556811d46feec3
wangyouzheng <353735348@qq.com>
2023-04-06 02:23:28 +0800

    init biz-modules

---
+++ boot3-biz-modules/biz-modules-system/src/main/java/com/boot3/system/domain/SysUserRole.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright(c) 2023-2025 QingDao Raise Technology Co., Ltd. All Rights Reserved.
+ */
+
+package com.boot3.system.domain;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * 用户和角色关联 sys_user_role
+ *
+ * @author Raise
+ */
+public class SysUserRole {
+    /**
+     * 用户ID
+     */
+    private Long userId;
+
+    /**
+     * 角色ID
+     */
+    private Long roleId;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("userId", getUserId())
+                .append("roleId", getRoleId())
+                .toString();
+    }
+}
---
+++ boot3-biz-modules/biz-modules-system/src/main/java/com/boot3/system/mapper/SysRoleMenuMapper.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright(c) 2023-2025 QingDao Raise Technology Co., Ltd. All Rights Reserved.
+ */
+
+package com.boot3.system.mapper;
+
+import com.boot3.system.domain.SysRoleMenu;
+
+import java.util.List;
+
+/**
+ * 角色与菜单关联表 数据层
+ *
+ * @author Raise
+ */
+public interface SysRoleMenuMapper {
+    /**
+     * 查询菜单使用数量
+     *
+     * @param menuId 菜单ID
+     * @return 结果
+     */
+    public int checkMenuExistRole(Long menuId);
+
+    /**
+     * 通过角色ID删除角色和菜单关联
+     *
+     * @param roleId 角色ID
+     * @return 结果
+     */
+    public int deleteRoleMenuByRoleId(Long roleId);
+
+    /**
+     * 批量删除角色菜单关联信息
+     *
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+    public int deleteRoleMenu(Long[] ids);
+
+    /**
+     * 批量新增角色菜单信息
+     *
+     * @param roleMenuList 角色菜单列表
+     * @return 结果
+     */
+    public int batchRoleMenu(List<SysRoleMenu> roleMenuList);
+}
