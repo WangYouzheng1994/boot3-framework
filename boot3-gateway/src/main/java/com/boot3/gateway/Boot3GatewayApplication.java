8ce0de9610e1c47adfffd0887c66777ee9d530ab
qingsong <qingsong@qq.com>
2023-04-06 17:42:26 +0800

    网关启动成功。

--- boot3-gateway/src/main/java/com/boot3/gateway/Boot3GatewayApplication.java
+++ boot3-gateway/src/main/java/com/boot3/gateway/Boot3GatewayApplication.java
@@ -6,8 +6,12 @@ package com.boot3.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+/**
+ * 网关 - 不需要数据源的自动装备，否则会报找不到url地址的配置的错误
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Boot3GatewayApplication {
 
     public static void main(String[] args) {
